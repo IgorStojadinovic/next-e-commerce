59ac11f260881147f32d78df3b18b4e601a85a14
Igor Stojadinovic <igorstojadinovic.vs@gmail.com>
2024-03-11 18:58:37 +0100

    update

--- app/store/slice.ts
+++ app/store/slice.ts
@@ -1,84 +1,86 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { CartItem, CounterState } from '@/lib/types'
-import { getLocalStore } from 'next-persist'
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { CartItem, CounterState } from "@/lib/types";
+import { getLocalStore } from "next-persist";
 
 const initialState: CounterState = {
   cart: [],
   quantity: 0,
-}
+  isOpen: false,
+  cartIsOpen: false,
+};
 
-const persistedState = getLocalStore('cart', initialState)
+const persistedState = getLocalStore("cart", initialState);
 
 export const cartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState,
   reducers: {
     increment: (state) => {
-      state.quantity += 1
+      state.quantity += 1;
 
-      console.log('persist', persistedState)
+      console.log("persist", persistedState);
     },
     decrement: (state) => {
-      state.quantity = Math.max(0, state.quantity - 1)
+      state.quantity = Math.max(0, state.quantity - 1);
     },
     addItemToCart: (state, action: PayloadAction<CartItem>) => {
       const existingItemIndex = state.cart.findIndex(
         (cartItem) => cartItem.name === action.payload.name
-      )
+      );
 
       if (existingItemIndex !== -1) {
-        const updatedCart = [...state.cart]
+        const updatedCart = [...state.cart];
         updatedCart[existingItemIndex] = {
           ...state.cart[existingItemIndex],
           total: state.cart[existingItemIndex].total + action.payload.total,
           quantity:
             state.cart[existingItemIndex].quantity + action.payload.quantity,
-        }
+        };
 
-        state.cart = updatedCart
-        state.quantity = 0
+        state.cart = updatedCart;
+        state.quantity = 0;
       } else {
-        state.cart.push(action.payload)
-        state.quantity = 0
+        state.cart.push(action.payload);
+        state.quantity = 0;
       }
     },
     addQuantity: (state, action: PayloadAction<CartItem>) => {
       const existingItemIndex = state.cart.findIndex(
         (cartItem) => cartItem.name === action.payload.name
-      )
+      );
       if (existingItemIndex !== -1) {
-        const updatedCart = [...state.cart]
+        const updatedCart = [...state.cart];
         updatedCart[existingItemIndex] = {
           ...state.cart[existingItemIndex],
           quantity: state.cart[existingItemIndex].quantity + 1,
           total:
             state.cart[existingItemIndex].total + action.payload.defaultPrice,
-        }
-        state.cart = updatedCart
-        state.quantity = 0
+        };
+        state.cart = updatedCart;
+        state.quantity = 0;
       }
     },
     removeQuantity: (state, action: PayloadAction<CartItem>) => {
       const existingItemIndex = state.cart.findIndex(
         (cartItem) => cartItem.name === action.payload.name
-      )
+      );
       if (existingItemIndex !== -1) {
-        const updatedCart = [...state.cart]
+        const updatedCart = [...state.cart];
         updatedCart[existingItemIndex] = {
           ...state.cart[existingItemIndex],
           quantity: state.cart[existingItemIndex].quantity - 1,
           total:
             state.cart[existingItemIndex].total - action.payload.defaultPrice,
-        }
-        state.cart = updatedCart.filter((item) => item.quantity > 0)
-        state.quantity = 0
+        };
+        state.cart = updatedCart.filter((item) => item.quantity > 0);
+        state.quantity = 0;
       }
     },
     removeAll: (state) => {
-      state.cart = []
+      state.cart = [];
     },
   },
-})
+});
 
 export const {
   increment,
@@ -87,6 +89,8 @@ export const {
   addQuantity,
   removeQuantity,
   removeAll,
-} = cartSlice.actions
-export const getValue = (state: CounterState) => state.quantity
-export default cartSlice.reducer
+} = cartSlice.actions;
+export const getValue = (state: CounterState) => state.quantity;
+export const getMenuStatus = (state: CounterState) => state.isOpen;
+export const getCartStatus = (state: CounterState) => state.cartIsOpen;
+export default cartSlice.reducer;
--- lib/types.ts
+++ lib/types.ts
@@ -1,83 +1,93 @@
-import { StaticImageData } from 'next/image'
-import { links } from './data'
+import { StaticImageData } from "next/image";
+import { links } from "./data";
 
-export type SectionName = (typeof links)[number]['name']
+export type SectionName = (typeof links)[number]["name"];
 export type SecondaryNavLinks = {
-  name: string
-  url: string
-  imgpath: StaticImageData
-  height: number
-  width: number
-  toggleMenu: () => void
-}
+  name: string;
+  url: string;
+  imgpath: StaticImageData;
+  height: number;
+  width: number;
+  toggleMenu: () => void;
+};
 
 export type Product = {
-  type: string
-  name: string
-  desc: string
-  link: string
-  mobile: StaticImageData
-  tablet: StaticImageData
-  desktop: StaticImageData
-  className: string
-}
+  type: string;
+  name: string;
+  desc: string;
+  link: string;
+  mobile: StaticImageData;
+  tablet: StaticImageData;
+  desktop: StaticImageData;
+  className: string;
+};
 
 export type Data = {
-  data: object
-}
+  data: object;
+};
 
 type BoxItem = {
-  quantity: string
-  name: string
-}
+  quantity: string;
+  name: string;
+};
 
 type GalleryImagesObj = {
-  id: string
-  photo: StaticImageData
-  className: string
-}
+  id: string;
+  photo: StaticImageData;
+  className: string;
+};
 
 type GalleryObj = {
-  name: string
-  images: GalleryImagesObj[]
-}
+  name: string;
+  images: GalleryImagesObj[];
+};
 
 type ProductsSuggestions = {
-  name: string
-  link: string
-  thumbImg: StaticImageData
-}
+  name: string;
+  link: string;
+  thumbImg: StaticImageData;
+};
 
 export type IndividualProduct = {
-  type: string
-  name: string
-  maindesc: string
-  descOne: string
-  descTwo: string
-  thumbImg: StaticImageData
-  price: number
-  box: BoxItem[]
-  gallery: GalleryObj[]
-  products: ProductsSuggestions[]
-}
+  type: string;
+  name: string;
+  maindesc: string;
+  descOne: string;
+  descTwo: string;
+  thumbImg: StaticImageData;
+  price: number;
+  box: BoxItem[];
+  gallery: GalleryObj[];
+  products: ProductsSuggestions[];
+};
 
 export type DispatchItem = {
-  name: string
-  total: number
-  defaultPrice: number
-  quantity: number
-  itemKey: string
-}
+  name: string;
+  total: number;
+  defaultPrice: number;
+  quantity: number;
+  itemKey: string;
+};
 
 export type CartItem = {
-  name: string
-  total: number
-  defaultPrice: number
-  quantity: number
-  itemKey: string
-}
+  name: string;
+  total: number;
+  defaultPrice: number;
+  quantity: number;
+  itemKey: string;
+};
+
+type MenuStatus = false | true;
+type LocalStorage = string;
+type Cart = [];
+
+type MainContextProviderProps = {
+  children: React.ReactNode;
+};
 
 export type CounterState = {
-  cart: CartItem[]
-  quantity: number
-}
+  isOpen: MenuStatus;
+  cartIsOpen: MenuStatus;
+  cart: CartItem[];
+  quantity: number;
+};
