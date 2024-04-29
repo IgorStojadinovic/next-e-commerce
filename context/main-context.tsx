413bce10dcb7e6a10946c3d1bf5e21bb1d9ee516
Igor Stojadinovic <igorstojadinovic.vs@gmail.com>
2024-04-29 10:21:19 +0200

    'refactored code'

--- context/main-context.tsx
+++ context/main-context.tsx
@@ -1,71 +1,70 @@
 "use client";
 
-import React, { useEffect, useState, createContext, useContext } from "react";
+import React, {createContext, useContext, useState} from "react";
 
 type MenuStatus = false | true;
-type LocalStorage = string;
 type Cart = [];
 
 type MainContextProviderProps = {
-  children: React.ReactNode;
+    children: React.ReactNode;
 };
 
 type MainContextType = {
-  isOpen: MenuStatus;
-  cartIsOpen: MenuStatus;
-  cart: Cart;
-  toggleMenu: () => void;
-  toggleCart: () => void;
+    isOpen: MenuStatus;
+    cartIsOpen: MenuStatus;
+    cart: Cart;
+    toggleMenu: () => void;
+    toggleCart: () => void;
 };
 
 const MainContext = createContext<MainContextType | null>(null);
 
 export default function MainContextProvider({
-  children,
-}: MainContextProviderProps) {
-  const [isOpen, setIsOpen] = useState<MenuStatus>(false);
-  const [cartIsOpen, setCartIsOpen] = useState<MenuStatus>(false);
-  const [cart] = useState<Cart>([]);
+                                                children,
+                                            }: MainContextProviderProps) {
+    const [isOpen, setIsOpen] = useState<MenuStatus>(false);
+    const [cartIsOpen, setCartIsOpen] = useState<MenuStatus>(false);
+    const [cart] = useState<Cart>([]);
 
-  const toggleMenu = () => {
-    if (isOpen) {
-      setIsOpen((prev) => !prev);
-    } else {
-      setIsOpen((prev) => !prev);
-    }
-  };
+    const toggleMenu = () => {
+        if (isOpen) {
+            setIsOpen((prev) => !prev);
+        } else {
+            setIsOpen((prev) => !prev);
+        }
+    };
 
-  const toggleCart = () => {
-    if (cartIsOpen) {
-      setCartIsOpen((prev) => !prev);
-    } else {
-      setCartIsOpen((prev) => !prev);
-    }
-  };
+    const toggleCart = () => {
+        if (cartIsOpen) {
+            setCartIsOpen((prev) => !prev);
+        } else {
+            setCartIsOpen((prev) => !prev);
+        }
+    };
 
-  return (
-    <MainContext.Provider
-      value={{
-        isOpen,
-        cartIsOpen,
-        cart,
-        toggleMenu,
-        toggleCart,
-      }}
-    >
-      {children}
-    </MainContext.Provider>
-  );
+    return (
+        <MainContext.Provider
+            value={{
+                isOpen,
+                cartIsOpen,
+                cart,
+                toggleMenu,
+                toggleCart,
+            }}
+        >
+            {children}
+        </MainContext.Provider>
+    );
 }
 
 export function useMainContextProvider() {
-  const context = useContext(MainContext);
+    const context = useContext(MainContext);
 
-  if (context === null) {
-    throw new Error(
-      "useMainContextProvider must be used within a MainContextProvider"
-    );
-  }
+    if (context === null) {
+        throw new Error(
+            "useMainContextProvider must be used within a MainContextProvider"
+        );
+    }
 
-  return context;
+    return context;
 }
